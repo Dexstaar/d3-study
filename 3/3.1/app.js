9744a47bf6552c4f84e281f8b77687a1e4816be6
Kim, JeongHoon | RASIA <jeonghoon.kim@rakuten.com>
2018-06-14 16:26:45 +0800

    done lec22

--- 3/3.1/app.js
+++ 3/3.1/app.js
@@ -1,2 +1,9 @@
-var el = d3.selectAll( 'body' ).append( 'p' );
+var el = d3.selectAll('body')
+    .append('p')
+    // .attr('class', 'foo')
+    // .attr('class', 'bar')
+    .text('Hello World!')
+    .classed('foo', true)
+    .classed('bar', true)
+    .style('color', 'blue');
 console.log(el);
