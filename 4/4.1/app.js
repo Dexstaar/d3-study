d27ad59af56c8c42c226c2ad8dc957d3eb1315a7
dexstar <dexstaar@gmail.com>
2018-06-17 19:01:17 +0800

    done lec30

--- 4/4.1/app.js
+++ 4/4.1/app.js
@@ -1,21 +1,54 @@
 var data  = [];
 
-for(var i=0; i<25; i++) {
+for(var i=0; i<20; i++) {
     // Math.round()
     var num = Math.floor(d3.randomUniform(1,50)());
     data.push(num);
 }
 
 // console.log(data);
+// Create SVG Element
+var chart_width = 800;
+var chart_height = 400;
+var bar_padding = 5;
 
-d3.select('#chart')
-    .selectAll('div')
+var svg = d3.select('#chart')
+    .append('svg')
+    .attr('width', chart_width)
+    .attr('height', chart_height);
+
+// Bind Data and Create Bars
+svg.selectAll('rect')
     .data(data)
     .enter()
-    .append('div')
-    //.calssed('bar', ture)
-    .attr('class','bar')
-    .style('height', function(d){
-        var height = d * 3;
-        return height + 'px';
-    });
+    .append('rect')
+    .attr('x', function(d, i){
+        return i * ( chart_width / data.length );
+    })
+    .attr('y', function(d){
+        return chart_height - d * 5;
+    })
+    .attr('width', chart_width / data.length - bar_padding )
+    .attr('height', function(d){
+        return d * 5;
+    })
+    .attr('fill', '#7ED26D');
+
+// Create Labels
+svg.selectAll('text')
+    .data(data)
+    .enter()
+    .append('text')
+    .text(function(d){
+        return d;
+    })
+    .attr('x', function(d, i){
+        return i * ( chart_width / data.length ) +
+            ( chart_width / data.length - bar_padding) / 2;
+    })
+    .attr('y', function(d){
+        return chart_height - d * 5 + 15;
+    })
+    .attr('font-size', 14)
+    .attr('fill', '#fff')
+    .attr('text-anchor', 'middle');
